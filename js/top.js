258dab1ea5e84c1653189659f9396167249ca5a1
rei-portfolio <87227727+rei-portfolio@users.noreply.github.com>
2021-11-21 17:53:37 +0900

    Add files via upload

--- js/top.js
+++ js/top.js
@@ -3,11 +3,11 @@ $(function () {
   jQuery(window).on("scroll", function () {
     if (jQuery("#js-header").height() < jQuery(this).scrollTop()) {
       jQuery("#js-header").addClass("headerColorScroll");
-      jQuery("#js-header-img").attr("src", "../img/logo.png");
+      jQuery("#js-header-img").attr("src", "https://rei-portfolio.github.io/ishii-kadan/img/logo.png");
       // jQuery("#js-hamburger").addClass("headerColorScroll-false");
     } else {
       jQuery("#js-header").removeClass("headerColorScroll");
-      jQuery("#js-header-img").attr("src", "../img/header-logo.svg");
+      jQuery("#js-header-img").attr("src", "https://rei-portfolio.github.io/ishii-kadan/img/logo.pngimg/header-logo.svg");
       // jQuery("#js-hamburger").removeClass("headerColorScroll-false");
     }
   });
